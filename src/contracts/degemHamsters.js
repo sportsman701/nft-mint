b8a57fc99074f37960059ab498e7b08b56d1dea0
sportsman701 <sportsman701@outlook.com>
2021-09-17 19:11:04 -0500

    Fix issues

--- src/contracts/degemHamsters.js
+++ src/contracts/degemHamsters.js
@@ -1,4 +1,4 @@
-import { ethers } from 'ethers';
+import { ethers, BigNumber } from 'ethers';
 import Degem_abi from './abi/degemHamsters.abi.json'
 import degemSecret from './abi/secret.json'
 
@@ -6,11 +6,11 @@ export async function adopt(number, signer) {
 
   const Degem = new ethers.Contract(degemSecret.DegenHamstersAddress, Degem_abi, signer);
 
-  Degem.adoptHamster(number);
+  await Degem.adoptHamster(number, {value: BigNumber.from("300000000000000000")});
   
   return new Promise((resolve, reject) => {
     Degem.on("Transfer", (from, to, tokenId) => {
-        console.log("tokenId", tokenId)
+      console.log("tokenId", tokenId)
       resolve(tokenId)
     });
   })
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,3 +5,13 @@ export const truncatedText = (sourceText) => {
     return sourceText
   }
 }
+
+export const getBigNumber = (source:any) => {
+  source += '';
+  const parts = source.split(".");
+  let decimals = 18;
+  if (parts[1] && parts[1].length) decimals -= parts[1].length;
+  let zero = "0";
+  if (decimals < 0) return parts[0] + parts[1].slice(0, 18);
+  return parts[0] + (parts[1]?parts[1]:"") + (zero.repeat(decimals));
+}
